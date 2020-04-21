eb9e1625fd9f72bbaeaf1c17960541f20459525b
Ashley Rang <bajadeashley@gmail.com>
2020-04-20 20:29:07 -0700

    fixed weird errors and rearranged code. added try/catch

--- function1.js
+++ function1.js
@@ -1,14 +1,25 @@
 
-function createGreeting(){ 
-    let name = "Mr. Bojangles";
-    return("Hello my name is " + name);
-}
 
-function getYearOfBirth(){
-    let age = 53;
-    const yob = 2020 - age;
-    return("I was born in" + yob);
+function getYearOfBirth(age){
+     return 2020 - age;
   }
+
+  function createGreeting(name, age) { 
+    if (age < 0){
+      throw new Error('Age can not be negative');}
+    
+      if (typeof age !== 'number') {
+          throw new TypeError('Age must be a number');}
+          
+    const yob = getYearOfBirth(age);
+    return `Hi, my name is ${name} and I\'m ${age} years old. I was born in ${yob}.`;
+}
   
-const greeting1 = createGreeting();
-console.log(greeting1);
+
+
+try {
+    const greeting1 = createGreeting('Mr. Bojangles', 53);
+  console.log(greeting1);
+}catch(e) {
+    console.error(e.message);
+}
