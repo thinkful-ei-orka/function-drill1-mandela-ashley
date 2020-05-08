df98429098e5eb28a628a257c131cea155f1f774
M Jones <jonesman2800@gmail.com>
2020-05-08 16:21:58 -0400

    force error for empty arguments

--- function1.js
+++ function1.js
@@ -1,10 +1,13 @@
 'use strict';
 
-// 7 Force an error for negative ages entered.
+// 7b Force an error if arguments not entered.
 function getYearOfBirth(age) {
   return 2020 - age;}
 
 function createGreeting(name, age) {
+  if(name === undefined || age === undefined) {
+    throw new Error('Arguments not valid. name must be a string. age must be positive number');
+  }
   if(age < 0) {
     throw new Error('Age can not be negative');
   }
@@ -12,7 +15,7 @@ function createGreeting(name, age) {
   return `Hi my name is ${name} and I'm ${age} years old. I was born in ${yearOfBirth}` ;}
 
 try {
-  const greeting1 = (createGreeting('Ghorb', -198)) ;
+  const greeting1 = (createGreeting( 'Gharu',-198)) ;
   console.log(greeting1);}
 catch(e) {console.error(e.message)}
 
