0715c530c6776cd05da7300059a85f8fb4272b54
M Jones <jonesman2800@gmail.com>
2020-05-08 15:47:14 -0400

    made function return hi statement

--- function1.js
+++ function1.js
@@ -1,9 +1,11 @@
 'use strict';
 
+// 3 Have function return and log returned value from variable
 function createGreeting() {
-  console.log("Hi my name is Dela and I'm 99 years old");
+  return "Hi my name is Dela and I'm 99 years old" ;
 }
 
 createGreeting();
 
-
+const greeting1 = (createGreeting()) ;
+console.log(greeting1);
