e115f444d3771f299080215871ff33e9dd8e5b7d
M Jones <jonesman2800@gmail.com>
2020-05-08 17:06:08 -0400

    created beyond function with error for non-numbers

--- function2.js
+++ function2.js
@@ -1,9 +1,34 @@
 // Function Drills 2
 'use strict';
 
+// Jedi name
 function jediName(firstName, lastName) {
-    return lastName.slice(0,3) + firstName.slice(0,2);
-}
+    return lastName.slice(0,3) + firstName.slice(0,2);}
 
 const myJediName = (jediName('Mandela','Jones'));
 console.log(myJediName);
+
+// To infinity...
+
+function beyond(num) {
+    if (typeof num !== 'number') {
+        throw new TypeError('Valid number required');}
+
+    if (num === 0) {
+        console.log('Staying home');}
+        
+    if (Number.isFinite(num) === true && num < 0 || num < 0) {
+        console.log('To negative infinity...');}
+
+    if (Number.isFinite(num) === true && num > 0) {
+        console.log('To infinity...');}
+
+    if (Number.isFinite(num) !== true) {
+        console.log('And beyond...');}
+}
+
+try{
+beyond('zero');}
+catch (e) {console.error(e.message);}
+
+//
