3c0467c5ad15542452ecb809d900cf91d64f38ea
M Jones <jonesman2800@gmail.com>
2020-05-08 15:41:43 -0400

    wrap console log hi statement in function

--- function1.js
+++ function1.js
@@ -1,27 +1,9 @@
 'use strict';
 
-console.log("Hi my name is Dela and I'm 99 years old")
+function createGreeting() {
+  console.log("Hi my name is Dela and I'm 99 years old");
+}
 
-// function getYearOfBirth(age){
-//      return 2020 - age;
-//   }
-
-//   function createGreeting(name, age) { 
-//     if (age < 0){
-//       throw new Error('Age can not be negative');}
-    
-//       if (typeof age !== 'number') {
-//           throw new TypeError('Age must be a number');}
-          
-//     const yob = getYearOfBirth(age);
-//     return `Hi, my name is ${name} and I\'m ${age} years old. I was born in ${yob}.`;
-// }
-  
+createGreeting();
 
 
-// try {
-//     const greeting1 = createGreeting('Mr. Bojangles', 53);
-//   console.log(greeting1);
-// }catch(e) {
-//     console.error(e.message);
-// }
