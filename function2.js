0550db88f691c3859d32d51a5bfc95bd191ac01a
M Jones <jonesman2800@gmail.com>
2020-05-08 22:50:42 -0400

    rock paper scisors function with switch statement

--- function2.js
+++ function2.js
@@ -28,7 +28,71 @@ function beyond(num) {
 }
 
 try{
-beyond('zero');}
+beyond(0);}
 catch (e) {console.error(e.message);}
 
-//
+// Cracking the code
+
+// function decode(word) {
+//     const decoded = [];
+
+//     word.forEach(tha, => 
+//         console.log(tha);
+//         // if (letter[index-1] === '' || letter[index-1] === undefined) {
+//         //     if (letter[index] === a){}
+
+//         //     if (letter[index] === b){}
+
+//         //     if (letter[index] === c){}
+
+//         //     if (letter[index] === d){}
+
+//         //     else{decoded.push(' ')}
+  
+//         ); 
+    
+//     return decoded.join('');}
+
+
+// const encodedMessage = 'craft block argon meter bells brown croon droop'
+// const decodedMessage = decode(encodedMessage);
+// console.log(decodedMessage)
+
+
+// How many days in a month
+
+
+// Rock, Paper, Scissors game with input of 1-3 as rock paper scissors
+
+const randomNo = Math.floor(Math.random() *3) +1;
+
+function roPaSci (oneToThree) {
+    const such = randomNo ;
+    const winner = 'winner here'
+    const loser = 'loser here'
+    console.log(oneToThree, such);
+    switch (oneToThree) {
+        case 1:
+            if (such === 3){
+                console.log(winner);};
+            if (such === 2){
+                console.log(loser);};
+            break;
+        case 2:
+            if(such === 1){
+                console.log(winner);};
+            if(such === 3){
+                console.log(loser);};
+            break;
+        case 3: 
+            if(such === 2) {
+                console.log(winner);};
+            if (such === 1){
+                console.log(loser);};
+            break;
+
+    default:
+        throw new Error('Enter 1 for Rock, 2 for Paper, 3 for Scissors');
+}};
+
+roPaSci(1);
