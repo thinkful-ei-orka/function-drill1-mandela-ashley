a7a024febf4a37dd2a0db54b5ce0984062f5b8e4
M Jones <jonesman2800@gmail.com>
2020-05-09 10:29:36 -0400

    function as argument 1

--- higherOrder.js
+++ higherOrder.js
@@ -1,1 +1,15 @@
 'use strict';
+
+
+// Functions as Arguments
+function repeat (fn,n) {
+    for (let i = 0; i < n; i++) {
+        fn; }}
+
+const hello = function () {return console.log('hello world');}
+
+// function goodbye () {console.log('goodbye world');}
+const goodby = 'goodbye world'
+
+// hello();
+repeat(hello(), 5);
